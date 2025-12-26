90759d474e5738078374abfb264d8cc89216be11
Maciej <maciej@MacBook-Air-Maciej.local>
2025-12-26 13:38:29 +0100

    fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -30,7 +30,7 @@ const Home = () => {
       <section className="grid w-full gap-8 home-section">
         <div className="h-full md:col-span-1 xl:col-span-1">
           <TradingViewWidget
-            title="Market Overview"
+            title="Top Stories"
             scriptUrl={`${scriptUrl}timeline.js`}
             config={TOP_STORIES_WIDGET_CONFIG}
             className="custom-chart"
@@ -39,7 +39,7 @@ const Home = () => {
         </div>
         <div className="h-full md:col-span-1 xl:col-span-2">
           <TradingViewWidget
-            title="Stock Heatmap"
+            title="Market data"
             scriptUrl={`${scriptUrl}market-quotes.js`}
             config={MARKET_DATA_WIDGET_CONFIG}
             height={600}
